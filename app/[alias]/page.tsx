6abc440116a7f45e502895eb64becdbb941017b7
AnaJulia Bortolossi <anajbdbl@bu.edu>
2025-04-18 20:52:18 -0400

    added files to finish mp-5

--- app/[alias]/page.tsx
+++ app/[alias]/page.tsx
@@ -1,10 +1,18 @@
-import { redirect } from "next/navigation";
-import { findAlias } from "@/lib/db";
+import { redirect } from 'next/navigation';
+import { findAlias } from '@/lib/models';
 
-export default async function RedirectAlias({ params }: { params: { alias: string } }) {
+interface AliasPageProps {
+  params: {
+    alias: string;
+  };
+}
+
+export default async function AliasRedirectPage({ params }: AliasPageProps) {
   const record = await findAlias(params.alias);
+
   if (!record) {
     return <h1>Alias not found</h1>;
   }
+
   redirect(record.url);
 }
