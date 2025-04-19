18b95a724b95e11bb1db051542f3ef2760bc8bf2
AnaJulia Bortolossi <anajbdbl@bu.edu>
2025-04-18 20:55:28 -0400

    changes

--- lib/models.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "./mongo";
-
-export async function findAlias(alias: string) {
-  return db.collection("urls").findOne({ alias });
-}
-
-export async function createAlias(alias: string, url: string) {
-  return db.collection("urls").insertOne({ alias, url });
-}
